00a00d86015ff4e620197961b180bbfdd191dfb8
abdgit1 <abdullahmansoorahmed999@gmail.com>
2026-07-23 11:58:34 +0500

    SEO metadata --> Static & Dynamic

--- app/movies/[id]/page.jsx
+++ app/movies/[id]/page.jsx
@@ -2,6 +2,24 @@ import {movies} from "../page.jsx";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 
+
+
+export async function generateMetadata({ params }) {
+    const { id } = await params;
+    const movie = movies.find((movie) => movie.id === parseInt(id));
+
+    if (!movie) {
+        return {
+            title: "Movie Not Found",
+            description: "The requested movie could not be found.",
+        };
+    }
+    return {
+        title: movie.title + " - Movie Explorer",
+        description: movie.description,
+    };
+}
+
 export default async function MovieDetails({ params }) {
   const { id } = await new Promise((resolve) => setTimeout(() => resolve(params), 1000)); // Simulate a delay for loading state
   const movie = movies.find((movie) => movie.id === parseInt(id));
